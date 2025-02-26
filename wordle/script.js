df0f64ed0e188c4ee277ff607173117765433065
Zsommbi200 <eszeki.zsombor@gmail.com>
2025-02-26 20:08:28 +0100

    asdwa

--- wordle/script.js
+++ wordle/script.js
@@ -4,12 +4,12 @@ const submit = document.getElementById("submit");
 const error = document.getElementById("error");
 const word = words[Math.floor(Math.random(0, words.length))];
 const letters = document.getElementById("inWord");
-console.log(word);
+const show = document.getElementById("word");
+let letterCharachter = [];
 
 function asd() {
   let guess = document.getElementById("input").value;
   let i = true;
-  console.log(guess);
 
   if (guess.length !== 5) {
     error.innerHTML = "Word must be 5 letters";
@@ -17,14 +17,22 @@ function asd() {
   } else if (!/^[A-Za-z]+$/) {
     error.innerHTML = "Word can only contain letters";
     i = false;
+  } else if (guess === word) {
+    error.innerHTML = "You win";
   }
-// not working 
-  guess
-    .split("")
-    .map((ele) =>
-      word.indexOf(ele) === -1
-        ? asd
-        : (letters.innerHTML += ` ${ele}`)
-    );
+//not working
+  guess.split("").map((ele, index) => {
+    if (word[index] === ele) {
+      show.innerHTML[index * 2 - 1] = ele;
+    }
+  });
+
+  guess.split("").map((ele) => {
+    if (word.indexOf(ele) !== -1 && letterCharachter.indexOf(ele) === -1) {
+      letters.innerHTML += ` ${ele}`;
+      letterCharachter.push(ele);
+    }
+  });
 }
+
 submit.onclick = asd;
