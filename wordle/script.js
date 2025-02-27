41d6c121748afc6c9c492554ffd2b6eb20c281c9
Zsommbi200 <eszeki.zsombor@gmail.com>
2025-02-27 10:11:49 +0100

    win screen needs to be added

--- wordle/script.js
+++ wordle/script.js
@@ -5,25 +5,34 @@ const error = document.getElementById("error");
 const word = words[Math.floor(Math.random(0, words.length))];
 const letters = document.getElementById("inWord");
 const show = document.getElementById("word");
+const wrongLetters = document.getElementById("wrongLetter");
+
 let letterCharachter = [];
+let wrongCharachters = [];
+let winCheck = false;
 
 function asd() {
-  let guess = document.getElementById("input").value;
-  let i = true;
+  let guess = document.getElementById("input").value.toLowerCase();
 
   if (guess.length !== 5) {
     error.innerHTML = "Word must be 5 letters";
-    i = false;
-  } else if (!/^[A-Za-z]+$/) {
+    return;
+  } else if (!/^[A-Za-z]+$/.test(guess)) {
     error.innerHTML = "Word can only contain letters";
-    i = false;
+    return;
   } else if (guess === word) {
     error.innerHTML = "You win";
+    winCheck = true
+  } else {
+    error.innerHTML = "Good guess"
   }
-//not working
+
   guess.split("").map((ele, index) => {
     if (word[index] === ele) {
-      show.innerHTML[index * 2 - 1] = ele;
+      show.innerText = show.innerHTML
+        .split(" ")
+        .map((char, i) => (i === index ? ele : char))
+        .join(" ");
     }
   });
 
@@ -31,8 +40,14 @@ function asd() {
     if (word.indexOf(ele) !== -1 && letterCharachter.indexOf(ele) === -1) {
       letters.innerHTML += ` ${ele}`;
       letterCharachter.push(ele);
+    } else if (word.indexOf(ele) === -1 && wrongCharachters.indexOf(ele) === -1) {
+      wrongLetters.innerHTML += ` ${ele}`
+      wrongCharachters.push(ele)
     }
   });
 }
 
 submit.onclick = asd;
+if (winCheck) {
+  
+}
