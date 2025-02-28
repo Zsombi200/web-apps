2fc0035cbec319ce57654e983150e1a6f625c8bc
Zsommbi200 <eszeki.zsombor@gmail.com>
2025-02-28 21:11:21 +0100

    add doesnt disappear

--- bank/script.js
+++ bank/script.js
@@ -1,1 +1,10 @@
-add
+const add = document.getElementById("add");
+const topBar = document.getElementById("fgh");
+const block = document.getElementById("window");
+
+function addOnHover() {
+    block.classList.toggle("show")
+    block.classList.toggle("hidden")
+    topBar.classList.toggle("hidden")
+}
+add.addEventListener("click", addOnHover);
