f5b53b11dca8fc9ef7590e09c855608f63db428b
Zsommbi200 <eszeki.zsombor@gmail.com>
2025-03-02 13:26:19 +0100

    animation needed for roll

--- bank/script.js
+++ bank/script.js
@@ -2,9 +2,16 @@ const add = document.getElementById("add");
 const topBar = document.getElementById("fgh");
 const block = document.getElementById("window");
 
-function addOnHover() {
-    block.classList.toggle("show")
-    block.classList.toggle("hidden")
-    topBar.classList.toggle("hidden")
+function addOnHover(event) {
+  event.stopPropagation();
+  block.classList.toggle("hidden");
+  topBar.classList.toggle("hidden");
 }
+
 add.addEventListener("click", addOnHover);
+document.addEventListener("click", function (e) {
+  if (!block.contains(e.target) && !block.classList.contains("hidden")) {
+    block.classList.toggle("hidden");
+    topBar.classList.toggle("hidden");
+  }
+});
---
+++ diceroll/script.js
@@ -0,0 +1,46 @@
+const btn = document.getElementById("roll");
+const dice = document.getElementById("dice");
+const previousRolls = document.getElementById("previousRolls");
+let count = 0;
+
+const diceSides = [
+  "&#9856;",
+  "&#9857;",
+  "&#9858;",
+  "&#9859;",
+  "&#9860;",
+  "&#9861;",
+];
+
+btn.addEventListener("click", () => {
+  count++;
+  const side = diceSides[Math.floor(Math.random() * 6)];
+  dice.innerHTML = side;
+
+  const element = document.createElement("li");
+
+  const rollNumber = document.createElement("h2");
+  rollNumber.innerHTML = `Roll ${count}:`;
+
+  const showRoll = document.createElement("p");
+  showRoll.innerHTML = side;
+
+  element.appendChild(rollNumber);
+  element.appendChild(showRoll);
+  previousRolls.appendChild(element);
+
+  previousRolls.style.width = "500px";
+
+  rollNumber.style.margin = "0"
+
+  element.style.backgroundColor = "rgb(177, 177, 177)";
+  element.style.borderRadius = "10px";
+  element.style.display = "flex";
+  element.style.justifyContent = "space-between"
+  element.style.alignItems = "center"
+  element.style.padding = "10px"
+  element.style.marginTop = "10px"
+
+  element.querySelector("p").style.fontSize = "3em"
+  element.querySelector("p").style.margin = "0"
+});
