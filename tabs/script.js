c67a640671a2b445b26f1efe8d88491b823e39e8
Zsommbi200 <eszeki.zsombor@gmail.com>
2025-03-04 19:59:28 +0100

    tabs done

---
+++ tabs/script.js
@@ -0,0 +1,14 @@
+const tabs = document.getElementsByClassName("tabs");
+
+Array.from(tabs).forEach((tab) => {
+  tab.addEventListener("click", () => {
+    Array.from(document.querySelectorAll("#contents > div")).forEach((content) => {
+      content.classList.remove("show");
+      content.classList.add("hide")
+    }
+    );
+
+    const text = document.getElementById(tab.id.replace("s", "content"));
+    text.classList.add("show");
+  });
+});
