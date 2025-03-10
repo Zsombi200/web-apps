6b98ce83f692cec41f123de730b39c9f8ea59959
Zsommbi200 <eszeki.zsombor@gmail.com>
2025-03-10 20:13:05 +0100

    the beginning of react

---
+++ my-first-react-app/src/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <div>
+      <h1>First test</h1>
+      <p>Test</p>
+      <nav>
+        <ol>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+        </ol>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
