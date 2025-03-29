94cad02af10326cd68c5bf66df2c265637731958
Zsommbi200 <eszeki.zsombor@gmail.com>
2025-03-29 08:28:16 +0100

    need to do error handeling

--- react_calculator/src/calculator/Calculator.jsx
+++ react_calculator/src/calculator/Calculator.jsx
@@ -1,52 +1,70 @@
-import React, { useState } from "react";
-import { useContext, createContext } from "react";
+import { useRef } from "react";
 import "./Calculator.css";
 
-const clickContext = createContext()
-
 function Calculator() {
+  const screenRef = useRef(null);
 
-  function handleClick(event){
-    let charachter = event.value
-    if (typeof charachter === "string") {
-      handleSymbol(charachter);
-    } else {
-      
+  function handleClick(event) {
+    let charachter = event.target.value;
+    let screenElement = screenRef.current;
+
+    if (event.target.tagName === "BUTTON") {
+      switch (charachter) {
+        case "reset":
+          screenElement.value = "";
+          break;
+        case "back":
+          screenElement.value = screenElement.value.slice(0, -1);
+          break;
+        case "equal":
+          screenElement.value = eval(screenElement.value);
+          break;
+        default:
+          screenElement.value += charachter;
+      }
     }
   }
-
-  function handleSymbol(charachter){
-
-  }
-
   return (
     <div>
       <div id="display">
-        <input id="screen" />
+        <input
+          id="screen"
+          ref={screenRef}
+        />
       </div>
-      <div className="buttons">
-        <button value={reset}>C</button>
-        <button value={back}>←</button>
-        <button value={divide}>÷</button>
-        <button value={multiply}>*</button>
+      <div
+        className="buttons"
+        onClick={handleClick}>
+        <button value="reset">C</button>
+        <button value="back">←</button>
+        <button value="/">÷</button>
+        <button value="*">X</button>
 
         <button value={7}>7</button>
         <button value={8}>8</button>
         <button value={9}>9</button>
-        <button value={plus}>+</button>
+        <button value="+">+</button>
 
         <button value={4}>4</button>
         <button value={5}>5</button>
         <button value={6}>6</button>
-        <button value={minus}>-</button>
+        <button value="-">-</button>
 
         <button value={1}>1</button>
         <button value={2}>2</button>
         <button value={3}>3</button>
 
-        <button id="zero" value={0}>0</button>
-        <button value={comma}>,</button>
-        <button id="equal" value={equal}>=</button>
+        <button
+          id="zero"
+          value={0}>
+          0
+        </button>
+        <button value=",">,</button>
+        <button
+          id="equal"
+          value="equal">
+          =
+        </button>
       </div>
     </div>
   );
